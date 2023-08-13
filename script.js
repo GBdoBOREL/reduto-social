7b045b5a9e8f55870b1c6a1eeaadbe647e466b9a
xiTTz <es.vito@hotmail.com>
2023-08-13 19:08:51 -0300

    nav bar adicionado, correções na responsividade e outras melhorias

--- script.js
+++ script.js
@@ -9,18 +9,18 @@ let images = document.querySelector("img")
 let imageIndex = 0, interValid;
 
 
-// function autoSlide(){
-//     interValid = setInterval(() => {
-//         if(imageIndex >= 0){
+function autoSlide() {
+    interValid = setInterval(() => {
+        if (imageIndex >= 0) {
 
-//             slideImage(++imageIndex)
-//         }else{
-//             slideImage(--imageIndex)
-//         }
-//     }, 3000);
-// }
+            slideImage(++imageIndex)
+        } else {
+            slideImage(--imageIndex)
+        }
+    }, 3000);
+}
 
-// autoSlide();
+autoSlide();
 
 
 //Função que vai mudar imagem
@@ -49,8 +49,6 @@ const updateClick = (e) => {
     imageIndex += e.target.id === "next" ? 1 : -1;
     slideImage(imageIndex);
 
-
-
     //Reinicia o slide automatico
     autoSlide();
 
@@ -67,6 +65,71 @@ wrapper.addEventListener("mouseover", () => clearInterval(interValid));
 wrapper.addEventListener("mouseleave", autoSlide);
 
 
+const slider = document.querySelector(".slides");
 
-//Slide, parte avaliações
+var sectionIndex = 0;
 
+
+//Menu Slider das avaliações, responsavel por mover as divs
+document.querySelectorAll(".avaliacoes label").forEach(function (indicador, ind) {
+    indicador.addEventListener('click', function () {
+        sectionIndex = ind;
+        if (window.innerWidth < 769) {
+            
+            slider.style.transform = 'translate(' + (sectionIndex) * -74.5 + "%)";
+            slider.style.transition = "0.6s";
+        } else {
+
+            // sectionIndex = ind;
+            slider.style.transform = 'translate(' + (sectionIndex) * -58 + "%)";
+            slider.style.transition = "0.6s";
+        }
+
+
+    });
+});
+
+const observer = new ResizeObserver(entries => {
+    entries = document.querySelector(".slide1").checked = true;
+});
+
+
+
+
+class MobileBarV{
+    constructor(Itens, navList, NavLinks){
+        this.Itens = document.querySelector(Itens);   
+        this.navList = document.querySelector(navList);   
+        this.NavLinks = document.querySelectorAll(NavLinks);
+        this.ActiveClass = "active";
+        
+        this.handleClick = this.handleClick.bind(this)
+
+    }
+
+
+    handleClick(){
+        this.navList.classList.toggle(this.ActiveClass)
+    }
+
+
+    addEventClick(){    
+        this.Itens.addEventListener("click", this.handleClick)
+        this.navList.addEventListener("click", this.handleClick)
+
+    }
+
+    init(){
+        if(this.Itens && this.NavLinks){
+            this.addEventClick();
+        }
+        return this;
+    }
+
+
+}
+
+const MobileBar = new MobileBarV(".bars-menu", ".itens", ".itens a")
+
+
+MobileBar.init();
